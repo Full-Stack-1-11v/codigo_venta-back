9969464ecd01e9bdcccb09c4ac93a9fee26642d5
Jason <jaso.aguilera@duocuc.cl>
2025-05-26 20:21:05 -0400

    arreglado

--- src/main/java/com/perfulandia/venta/repository/VentaRepository.java
+++ src/main/java/com/perfulandia/venta/repository/VentaRepository.java
@@ -3,4 +3,10 @@ package com.perfulandia.venta.repository;
 import com.perfulandia.venta.model.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VentaRepository extends JpaRepository<Venta, Long> { }
+import java.util.List;
+
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+    List<Venta> findByClienteId(Long clienteId);
+}
+
+
